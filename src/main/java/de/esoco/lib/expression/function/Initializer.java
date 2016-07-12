8fca00cfcc34b997dcd0e382c056757ac990e18f
Elmar Sonnenschein <eso@esoco.de>
2016-07-12 13:19:29 +0200

    OPT: Initializer.init() throws Exception

--- src/main/java/de/esoco/lib/expression/function/Initializer.java
+++ src/main/java/de/esoco/lib/expression/function/Initializer.java
@@ -1,3 +1,19 @@
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This file is a part of the 'objectrelations' project.
+// Copyright 2016 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 package de.esoco.lib.expression.function;
 
 /********************************************************************
@@ -7,12 +23,14 @@ package de.esoco.lib.expression.function;
  */
 public interface Initializer<T>
 {
-	//~ Methods ------------------------------------------------------------
+	//~ Methods ----------------------------------------------------------------
 
 	/***************************************
 	 * Initializes the given object.
 	 *
-	 * @param rInitObject The object to initialize
+	 * @param  rInitObject The object to initialize
+	 *
+	 * @throws Exception If the initialization fails
 	 */
-	public void init(T rInitObject);
+	public void init(T rInitObject) throws Exception;
 }
