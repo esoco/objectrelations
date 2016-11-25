bd307db7b08481690d59b0cb32b468cf0afb0fd5
Elmar Sonnenschein <eso@esoco.de>
2016-11-25 11:11:40 +0100

    Java 8 support

--- src/main/java/org/obrel/core/RelationTypes.java
+++ src/main/java/org/obrel/core/RelationTypes.java
@@ -6,7 +6,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	  http://www.apache.org/licenses/LICENSE-2.0
+//		 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -467,7 +467,10 @@ public class RelationTypes
 		}
 
 		RelationType<Map<K, V>> aType =
-			newRelationType(sName, Map.class, fInitialValue, rModifiers);
+			newRelationType(sName,
+							(Class<? super Map<K, V>>) Map.class,
+							fInitialValue,
+							rModifiers);
 
 		aType.set(KEY_DATATYPE, rKeyType);
 		aType.set(VALUE_DATATYPE, rValueType);
