7a81f1348069fcb817084d0888acd449ecc91c07
Elmar Sonnenschein <eso@esoco.de>
2019-01-30 17:18:14 +0100

    OPT: error message

--- src/main/java/de/esoco/lib/expression/FunctionException.java
+++ src/main/java/de/esoco/lib/expression/FunctionException.java
@@ -1,6 +1,6 @@
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // This file is a part of the 'objectrelations' project.
-// Copyright 2018 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
+// Copyright 2019 Elmar Sonnenschein, esoco GmbH, Flensburg, Germany
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -50,7 +50,7 @@ public class FunctionException extends RuntimeException
 	 */
 	public FunctionException(Object fCausingFunction, Throwable eCause)
 	{
-		super(eCause);
+		super(eCause.getMessage(), eCause);
 
 		this.fCausingFunction = fCausingFunction;
 	}
